8f5167d1e74e829fcd9c4f91066bfd615152512a
ngmingch <ngmingch@gmail.com>
2015-04-12 23:30:11 +0800

    Project submission

--- Exploring-Data-Analysis/plot3.R
+++ Exploring-Data-Analysis/plot3.R
@@ -1,30 +1,30 @@
 plot3<-function() {
-  ##Read data
+  #Open and read data file
   pwr <- read.csv2("household_power_consumption.txt")
-  #Filter out the data between 1 Feb and 2 Feb 2007
-  pwr$DateTime<-strptime(paste(pwr$Date, pwr$Time), format ="%d/%m/%Y %H:%M:%S")
-  pwr<-pwr[pwr$DateTime > "2007-01-31 23:59:59" & pwr$DateTime < "2007-02-03 00:00:00",]
   
-  x<- pwr[,"DateTime"]
-  s1<- as.double(as.character(pwr[,"Sub_metering_1"]))
-  s2<- as.double(as.character(pwr[,"Sub_metering_2"]))
-  s3<- as.double(as.character(pwr[,"Sub_metering_3"]))
-  ylim_max<-max(s1)
+  #Extract the data between 1 Feb and 2 Feb 2007
+  pwr$datetime<-strptime(paste(pwr$Date, pwr$Time), format ="%d/%m/%Y %H:%M:%S")
+  pwr<-pwr[pwr$datetime > "2007-01-31 23:59:59" & pwr$datetime < "2007-02-03 00:00:00",]
   
-  #save plot PNG
+  datetime<- pwr[,"datetime"]
+  sub1<- as.double(as.character(pwr[,"Sub_metering_1"]))
+  sub2<- as.double(as.character(pwr[,"Sub_metering_2"]))
+  sub3<- as.double(as.character(pwr[,"Sub_metering_3"]))
+  y_max<-max(sub1)
+  
+  #save the plot in PNG file
   png("plot3.png", width=480, height=480, units="px")
-  plot (x, s1, type="n", ylab="Energy sub metering", xlab="", ylim=c(0, ylim_max))
-  lines(x,s1,col="black")
-  par(new=T) # suppress refresh canvas
+  plot (datetime, sub1, type="n", xlab="", ylab="Energy sub metering", ylim=c(0, y_max))
+  lines(datetime, sub1, col="black")
   
   #the next plot does not need to re-label and show the axes since the previous canvas is not refreshed
-  plot (x, s2, type="n", ylab="", xlab="", ylim=c(0, ylim_max), axes=F)
-  lines(x,s2,col="red")
-  par(new=T) # suppress refresh canvas
+  par(new=T)   # do not refresh canvas
+  plot (datetime, sub2, type="n", xlab="", ylab="", ylim=c(0, y_max), axes=F)
+  lines(datetime, sub2, col="red")
   
-  plot (x, s3, type="n", ylab="", xlab="", ylim=c(0, ylim_max), axes=F)
-  lines(x,s3,col="blue")
-  par(new=T) # suppress refresh canvas
+  par(new=T)
+  plot (datetime, sub3, type="n", xlab="", ylab="", ylim=c(0, y_max), axes=F)
+  lines(datetime, sub3, col="blue")
   
   legend ("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
   
